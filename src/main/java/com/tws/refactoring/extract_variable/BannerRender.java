b91e96033fe733cd205cc2666f74eca506138af0
aaron <kenzzw@163.com>
2019-07-15 17:02:18 +0800

    Refactor: refactor BannerRender Class

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -1,11 +1,12 @@
 package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
+
+    boolean isContainsMACandIE(String platform, String browser){
+        return (platform.toUpperCase().contains("MAC")) && (browser.toUpperCase().contains("IE"));
+    }
+
     String renderBanner(String platform, String browser) {
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1)) {
-            return "IE on Mac?";
-        }
-        return "banner";
+        return isContainsMACandIE(platform,browser)?"IE on Mac?":"banner";
     }
 }
