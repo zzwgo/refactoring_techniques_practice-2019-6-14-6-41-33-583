cf194525bd86a9323c1ccb6c6ed1c999dec1208c
aaron <kenzzw@163.com>
2019-07-15 18:05:30 +0800

    Refactor: refactor PriceCalculator Class

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -1,10 +1,16 @@
 package com.tws.refactoring.extract_variable;
 
 public class PriceCalculator {
+    private static final double FIVE_DISCOUNT=0.05;
+    private static final double QUANTITY=500;
+    private static final double MIN_COST=100.0;
+    private static final double ONE_DISCOUNT=0.1;
+    double calculatePrice(int quantity, int itemPrice){
+        double discount=quantity * itemPrice - Math.max(0, quantity - QUANTITY) * itemPrice * FIVE_DISCOUNT;
+        double shippingCost=Math.min(quantity * itemPrice * ONE_DISCOUNT, MIN_COST);
+        return  discount+shippingCost;
+    }
     double getPrice(int quantity, int itemPrice) {
-        // Price consists of: base price - discount + shipping cost
-        return quantity * itemPrice -
-                Math.max(0, quantity - 500) * itemPrice * 0.05 +
-                Math.min(quantity * itemPrice * 0.1, 100.0);
+        return calculatePrice(quantity,itemPrice);
     }
 }
