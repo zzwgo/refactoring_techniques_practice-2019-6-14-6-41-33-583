fcd476435804291b2166ff81d32f4ea3c9eb3385
aaron <kenzzw@163.com>
2019-07-15 21:31:25 +0800

    Refactor: refactor PriceCalculatorTest Class

--- src/test/java/com/tws/refactoring/extract_variable/PriceCalculatorTest.java
+++ src/test/java/com/tws/refactoring/extract_variable/PriceCalculatorTest.java
@@ -1,26 +1,35 @@
 package com.tws.refactoring.extract_variable;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class PriceCalculatorTest {
+    private PriceCalculator priceCalculator;
+
+    @Before
+    public void initPriceCalculator(){
+        priceCalculator=new PriceCalculator();
+    }
+
     @Test
     public void should_return_10100point0_when_input_100_and_100(){
-        PriceCalculator priceCalculator=new PriceCalculator();
         double result=priceCalculator.getPrice(100,100);
+
         assertEquals(10100.0,result,0.0);
     }
     @Test
     public void should_return_50100point0_when_input_500_and_100(){
-        PriceCalculator priceCalculator=new PriceCalculator();
         double result=priceCalculator.getPrice(500,100);
+
         assertEquals(50100.0,result,0.0);
     }
     @Test
     public void should_return_0point0_when_input_0_and_0(){
-        PriceCalculator priceCalculator=new PriceCalculator();
         double result=priceCalculator.getPrice(0,0);
+
         assertEquals(0.0,result,0.0);
     }
+
 }
