19a13c768d9ad64ab0f690414598d1c348c39a63
aaron <kenzzw@163.com>
2019-07-15 21:35:04 +0800

    Refactor: refactor BannerRenderTest Class

--- src/test/java/com/tws/refactoring/extract_variable/BannerRenderTest.java
+++ src/test/java/com/tws/refactoring/extract_variable/BannerRenderTest.java
@@ -1,21 +1,31 @@
 package com.tws.refactoring.extract_variable;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BannerRenderTest {
+    private BannerRender bannerRender;
+    @Before
+    public void initPriceCalculator(){
+         bannerRender=new BannerRender();
+    }
 
     @Test
     public void should_return_banner_when_input_abc_and_edf(){
-        BannerRender bannerRender=new BannerRender();
         String result=bannerRender.renderBanner("abc","edf");
         assertEquals("banner",result);
     }
 
     @Test
+    public void should_return_banner_when_input_MAC1_and_IE1(){
+        String result=bannerRender.renderBanner("MAC1","IE1");
+        assertEquals("IE on Mac?",result);
+    }
+
+    @Test
     public void should_return_banner_when_input_MAC_and_IE(){
-        BannerRender bannerRender=new BannerRender();
         String result=bannerRender.renderBanner("MAC","IE");
         assertEquals("IE on Mac?",result);
     }
