21074467777ab73de154d46fe7d9bd071f30f5af
aaron <kenzzw@163.com>
2019-07-15 16:56:55 +0800

    Test: add BannerRender Test

---
+++ src/test/java/com/tws/refactoring/extract_variable/PriceCalculatorTest.java
@@ -0,0 +1,22 @@
+package com.tws.refactoring.extract_variable;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PriceCalculatorTest {
+
+    @Test
+    public void should_return_banner_when_input_abc_and_edf(){
+        BannerRender bannerRender=new BannerRender();
+        String result=bannerRender.renderBanner("abc","edf");
+        assertEquals("banner",result);
+    }
+
+    @Test
+    public void should_return_banner_when_input_MAC_and_IE(){
+        BannerRender bannerRender=new BannerRender();
+        String result=bannerRender.renderBanner("MAC","IE");
+        assertEquals("IE on Mac?",result);
+    }
+}
